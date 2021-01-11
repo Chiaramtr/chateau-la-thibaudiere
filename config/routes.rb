27a047d140cd2149af4b09c200d58785545c7b12
Chiara de Montlaur <chiarademontlaur@gmail.com>
2021-01-10 21:16:08 -0300

    infos page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   post'contact', to: 'pages#contact'
+  get "infos", to: 'pages#infos', as: :infos
 
   # Generic syntax:
   # verb 'path', to: 'controller#action'
